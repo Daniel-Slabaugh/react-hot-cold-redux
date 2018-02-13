3eb65978804969d7bbd70a8e2909c3fa12f2b5ac
Daniel-Slabaugh <danielslabaugh@gmail.com>
2018-02-12 16:16:48 -0800

    top-nav test now worlking

--- src/components/top-nav.test.js
+++ src/components/top-nav.test.js
@@ -1,30 +1,44 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import {connect} from 'react-redux';
 
-import TopNav from './top-nav';
+import {generateAuralUpdate, restartGame} from '../actions';
 
-describe('<TopNav />', () => {
-  it('Renders without crashing', () => {
-    shallow(<TopNav />);
-  });
+import './top-nav.css';
 
-  it('Should call onNewGame when new game is clicked', () => {
-    const callback = jest.fn();
-    const wrapper = shallow(<TopNav onRestartGame={callback} />);
-    const link = wrapper.find('.new');
-    link.simulate('click', {
-      preventDefault() {}
-    });
-    expect(callback).toHaveBeenCalled();
-  });
+export function TopNav(props) {
+    return (
+        <nav>
+            <ul className="clearfix">
+                <li>
+                    <a href="#what" className="what" aria-label="How to play">
+                        What?
+                    </a>
+                </li>
+                <li>
+                    <a
+                        href="#feedback"
+                        className="new"
+                        aria-label="Start a new game"
+                        onClick={() =>
+                            props.dispatch(
+                                restartGame(Math.floor(Math.random() * 100) + 1)
+                            )}>
+                        + New Game
+                    </a>
+                </li>
+                <li>
+                    <a
+                        href="#get-status"
+                        /* the `visuallyhidden` class hides an element 
+            while leaving it available to screen reader users  */
+                        className="visuallyhidden focusable status-link"
+                        onClick={() => props.dispatch(generateAuralUpdate())}>
+                        Hear state of game
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    );
+}
 
-  it('Should call onGenerateAuralUpdate when state-of-game link is clicked', () => {
-    const callback = jest.fn();
-    const wrapper = shallow(<TopNav onGenerateAuralUpdate={callback} />);
-    const link = wrapper.find('.status-link');
-    link.simulate('click', {
-      preventDefault() {}
-    });
-    expect(callback).toHaveBeenCalled();
-  });
-});
+export default connect()(TopNav);
